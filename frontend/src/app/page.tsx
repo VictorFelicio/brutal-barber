a037cb1c7108f5c9a1008b3efb2556eef2bc7128
Victor Felicio <victor_felicio@hotmail.com>
2025-02-15 18:09:50 -0300

    feat: create section to background

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,20 @@
 import { OurProfessional } from '@/components/Professional/components/OurProfessional';
 import { OurServices } from '@/components/Service/components/OurServices';
+import { SectionBackground } from '@/components/shared/SectionBackground';
 
 export default function Landing() {
     return (
-        <div className="flex flex-col gap-10 container">
-            <h1 className="text-6xl py-20">Landing Teste</h1>
-            <OurServices />
-            <OurProfessional />
+        <div className="flex flex-col">
+            <div className="container py-10">
+                <h1 className="text-6xl py-20">Landing Teste</h1>
+            </div>
+            <SectionBackground image="/banners/servicos.webp">
+                <OurServices />
+            </SectionBackground>
+
+            <SectionBackground image="/banners/profissionais.webp">
+                <OurProfessional />
+            </SectionBackground>
         </div>
     );
 }
