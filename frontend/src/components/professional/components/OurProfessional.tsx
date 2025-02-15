670dc87ffc0bc4efab157f44ff76a0f7e7d719b1
Victor Felicio <victor_felicio@hotmail.com>
2025-02-15 17:51:39 -0300

    feat: create title for sections

--- frontend/src/components/professional/components/OurProfessional.tsx
+++ frontend/src/components/professional/components/OurProfessional.tsx
@@ -1,15 +1,23 @@
 import { professionals } from '@/data/constants/professionals';
 import { ProfessionalCard } from './ProfessionalCard';
+import { TitleSection } from '@/components/shared/TitleSection';
 
 export function OurProfessional() {
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
-            {professionals.map((professional) => (
-                <ProfessionalCard
-                    key={professional.id}
-                    professional={professional}
-                />
-            ))}
+        <div className="flex flex-col gap-16">
+            <TitleSection
+                tag="Time"
+                main="Nossos Brutos"
+                secondary="Só os mais brabos aqui! Temos o orgulho de ter o time mais qualificado do Brasil!"
+            />
+            <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
+                {professionals.map((professional) => (
+                    <ProfessionalCard
+                        key={professional.id}
+                        professional={professional}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
